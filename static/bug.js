c5c7d7ad8ea71dc197ed62de91bec3fb2207d0aa
Stuart Cleaver <sjmcleaver@gmail.com>
2025-02-27 18:56:47 -0800

    added bug knees

--- static/bug.js
+++ static/bug.js
@@ -2,26 +2,7 @@
 function inventBug(config, color, canvas, muted = false) {
 
 
-  // TODO: procedurally animated controllable avatars synchronized across peers
-
-  // TODO:
-  // leg movement is broken, logic is non-isotropic i think (atan?)
-  // backwards should also be reverse of forwards maybe
-  // i think knees are achievable
-  // time to port orb simulator?
-
-  // knees are circle intersections
-  // x position of intersection of unit circle with radius R circle at (A, B) is the solution of
-  // x^2(4A^2 + 4B^2) + x(-8AB^2 - 4AC) + C^2 - 4B^2(R^2 - A^2) = 0
-  // where C := 1 - R^2 - B^2 + A^2
-  // both legs are the same length so we can set R = 1 => C = A^2 - B^2
-  // x^2(4A^2 + 4B^2) + x(-8AB^2 - 4A(A^2 - B^2)) + (A^2 - B^2)^2 - 4B^2(1 - A^2) = 0
-
-  function intersect(A, B) {
-    let a = 4 * (A ** 2 + B ** 2),
-      b = -8 * A * B ** 2 - 4 * A * (A ** 2 - B ** 2),
-      c = (A ** 2 - B ** 2) ** 2 - 4 * (B ** 2)(1 - A ** 2);
-
+  function quadratic(a, b, c) {
     if (4 * a * c > b ** 2) { return false; }
     return [
       (-b + Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a),
@@ -29,8 +10,77 @@ function inventBug(config, color, canvas, muted = false) {
     ];
   }
 
-  // 
+  function intersect(A, B) {
 
+    // intersection of { p : |p| = 1 } with { p : |p - (A, B)| = 1 }
+    // general circle intersection of unit circle with radius R circle at (A, B):
+    //   x^2(4A^2 + 4B^2) + x(-8AB^2 - 4A(1 - R^2 - B^2 + A^2)) + (1 - R^2 - B^2 + A^2)^2 - 4B^2(1 - A^2) = 0
+    // taking R = 1
+    //   x^2(4A^2 + 4B^2) + x(-8AB^2 - 4A(A^2 - B^2)) + (A^2 - B^2)^2 - 4B^2(1 - A^2) = 0
+
+    let a = 4 * (A ** 2 + B ** 2),
+      b = -8 * A * B ** 2 - 4 * A * (A ** 2 - B ** 2),
+      c = (A ** 2 - B ** 2) ** 2 - 4 * (B ** 2) * (1 - A ** 2);
+
+    return quadratic(a, b, c);
+  }
+
+  function eq(x, y) {
+    return Math.abs(x - y) < 0.0001;
+  }
+
+  function knee(S, F, K) {
+    const R = (config.leg_max_length / 2),
+      A = (F.x - S.x) / R,
+      B = (F.y - S.y) / R,
+      intersection = intersect(A, B),
+      y1 = Math.sqrt(1 - intersection[0] ** 2),
+      y2 = Math.sqrt(1 - intersection[1] ** 2),
+      w1 = B + Math.sqrt(1 - (intersection[0] - A) ** 2),
+      w2 = B + Math.sqrt(1 - (intersection[1] - A) ** 2),
+      z1 = B - Math.sqrt(1 - (intersection[0] - A) ** 2),
+      z2 = B - Math.sqrt(1 - (intersection[1] - A) ** 2),
+      n1 = { x: intersection[0] },
+      n2 = { x: intersection[1] };
+
+      let p;
+
+      if (eq(y1, w1) || eq(y1, z1)) {
+        n1.y = y1
+      } else if (eq(-y1, w1) || eq(-y1, z1)) {
+        n1.y = -y1;
+      } else {
+        console.log("can't find intersection");
+        console.log(A, B, intersection);
+        return false;
+      }
+
+      if (eq(y2, w2) || eq(y2, z2)) {
+        n2.y = y2;
+      } else if (eq(-y2, w2) || eq(-y2, z2)) {
+        n2.y = -y2;
+      } else {
+        console.log("can't find intersection");
+        console.log(A, B, intersection);
+        return false;
+      }
+
+      const k = {
+        x: (K.x - S.x) / R,
+        y: (K.y - S.y) / R
+      };
+
+      if ((n1.x - k.x) ** 2 + (n1.y - k.y) ** 2 <= (n2.x - k.x) ** 2 + (n2.y - k.y) ** 2) {
+        p = n1;
+      } else {
+        p = n2;
+      }
+
+      return {
+        x: p.x * R + S.x,
+        y: p.y * R + S.y
+      };
+  }
 
   function direction(fromP, toP) {
     let dx = toP.x - fromP.x;
@@ -97,7 +147,14 @@ function inventBug(config, color, canvas, muted = false) {
     for (let i = 0; i < N; i++) {
       let x = p.x;
       let y = p.y + config.arc_radius * i;
-      nextBug.head.position = { x: x, y: y, llx: x - 50, lly: y, rlx: x + 50, rly: y };
+      nextBug.head.position = {
+        x: x,
+        y: y,
+        lf: { x: x - 50, y: y},
+        rf: { x: x + 50, y: y },
+        lk: { x: x, y: y },
+        rk: { x: x, y: y }
+      };
       nextBug.head.radius = R;
       if (i < N - 1) {
         nextBug.tail = { head: {} };
@@ -111,11 +168,11 @@ function inventBug(config, color, canvas, muted = false) {
   function moveBug(bug, p) {
     bug.head.position.x = p.x;
     bug.head.position.y = p.y;
-    if (p.llx) {
-      bug.head.position.llx = p.llx;
-      bug.head.position.lly = p.lly;
-      bug.head.position.rlx = p.rlx;
-      bug.head.position.rly = p.rly;
+    if (!bug.root) {
+      bug.head.position.lf = p.lf;
+      bug.head.position.rf = p.rf;
+      bug.head.position.lk = p.lk;
+      bug.head.position.rk = p.rk;
     }
 
     if (!bug.tail) {
@@ -123,31 +180,50 @@ function inventBug(config, color, canvas, muted = false) {
     }
 
     let d = direction(bug.head.position, bug.tail.head.position);
-    let ldx = bug.tail.head.position.llx - bug.tail.head.position.x;
-    let ldy = bug.tail.head.position.lly - bug.tail.head.position.y;
-    let rdx = bug.tail.head.position.rlx - bug.tail.head.position.x;
-    let rdy = bug.tail.head.position.rly - bug.tail.head.position.y;
+    let ldx = bug.tail.head.position.lf.x - bug.tail.head.position.x;
+    let ldy = bug.tail.head.position.lf.y - bug.tail.head.position.y;
+    let rdx = bug.tail.head.position.rf.x - bug.tail.head.position.x;
+    let rdy = bug.tail.head.position.rf.y - bug.tail.head.position.y;
 
     if (ldx ** 2 + ldy ** 2 > config.leg_max_length ** 2) {
-      bug.tail.head.position.llx = bug.tail.head.position.x + rotate(d, Math.PI / 4).x * config.leg_min_length;
-      bug.tail.head.position.lly = bug.tail.head.position.y + rotate(d, Math.PI / 4).y * config.leg_min_length;
+      bug.tail.head.position.lf.x = bug.tail.head.position.x + rotate(d, Math.PI / 4).x * config.leg_min_length;
+      bug.tail.head.position.lf.y = bug.tail.head.position.y + rotate(d, Math.PI / 4).y * config.leg_min_length;
       tap();
     }
 
     if (rdx ** 2 + rdy ** 2 > config.leg_max_length ** 2) {
-      bug.tail.head.position.rlx = bug.tail.head.position.x + rotate(d, -Math.PI / 4).x * config.leg_min_length;
-      bug.tail.head.position.rly = bug.tail.head.position.y + rotate(d, -Math.PI / 4).y * config.leg_min_length;
+      bug.tail.head.position.rf.x = bug.tail.head.position.x + rotate(d, -Math.PI / 4).x * config.leg_min_length;
+      bug.tail.head.position.rf.y = bug.tail.head.position.y + rotate(d, -Math.PI / 4).y * config.leg_min_length;
       tap();
     }
 
     let nextPosition = {
       x: bug.head.position.x + d.x * bug.head.radius,
       y: bug.head.position.y + d.y * bug.head.radius,
-      llx: bug.tail.head.position.llx,
-      lly: bug.tail.head.position.lly,
-      rlx: bug.tail.head.position.rlx,
-      rly: bug.tail.head.position.rly
+      lf: bug.tail.head.position.lf,
+      rf: bug.tail.head.position.rf
     };
+
+    const LK = knee(
+      bug.tail.head.position,
+      bug.tail.head.position.lf,
+      bug.tail.head.position.lk
+    ) || {
+      x: bugtail.head.position.x,
+      y: bugtail.head.position.y
+    };
+
+    const RK = knee(
+      bug.tail.head.position,
+      bug.tail.head.position.rf,
+      bug.tail.head.position.rk
+    ) || {
+      x: bugtail.head.position.x,
+      y: bugtail.head.position.y
+    };
+
+    nextPosition.lk = LK;
+    nextPosition.rk = RK;
 
     return {
       head: bug.head,
@@ -200,8 +276,17 @@ function inventBug(config, color, canvas, muted = false) {
           ctx,
           next.head.position.x - config.segment_radius,
           next.head.position.y - config.segment_radius,
-          next.head.position.llx,
-          next.head.position.lly,
+          next.head.position.lk.x,
+          next.head.position.lk.y,
+          config.leg_width,
+          segmentColor
+        );
+        drawLine(
+          ctx,
+          next.head.position.lk.x,
+          next.head.position.lk.y,
+          next.head.position.lf.x,
+          next.head.position.lf.y,
           config.leg_width,
           segmentColor
         );
@@ -209,14 +294,23 @@ function inventBug(config, color, canvas, muted = false) {
           ctx,
           next.head.position.x - config.segment_radius,
           next.head.position.y - config.segment_radius,
-          next.head.position.rlx,
-          next.head.position.rly,
+          next.head.position.rk.x,
+          next.head.position.rk.y,
+          config.leg_width,
+          segmentColor
+        );
+        drawLine(
+          ctx,
+          next.head.position.rk.x,
+          next.head.position.rk.y,
+          next.head.position.rf.x,
+          next.head.position.rf.y,
           config.leg_width,
           segmentColor
         );
 
-        drawCircle(ctx, next.head.position.llx, next.head.position.lly, config.foot_size, segmentColor);
-        drawCircle(ctx, next.head.position.rlx, next.head.position.rly, config.foot_size, segmentColor);
+        drawCircle(ctx, next.head.position.lf.x, next.head.position.lf.y, config.foot_size, segmentColor);
+        drawCircle(ctx, next.head.position.rf.x, next.head.position.rf.y, config.foot_size, segmentColor);
 
         const d = [lastCenter[0] - center[0], lastCenter[1] - center[1]];
         const p = [center[0] + d[0], center[1] + d[1]];
